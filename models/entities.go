d6b115ca132e5c9be54688973fd45458dcd8562f
jose-cleiton <cleitons835@gmail.com>
2023-01-01 13:48:59 -0300

    feat: :sparkles: Entities
    No código, o pacote "models" define uma estrutura de dados chamada "Todo" que representa uma tarefa "to-do". A estrutura contém os seguintes campos:  Id: um int64 que representa o ID da tarefa Title: uma string que representa o título da tarefa Description: uma string que representa a descrição da tarefa Done: um booleano que indica se a tarefa foi concluída ou não As anotações json:"id", json:"title", etc. são usadas para especificar os nomes dos campos quando a estrutura é convertida em um objeto JSON.

---
+++ models/entities.go
@@ -0,0 +1,14 @@
+package models
+
+
+
+
+type Todo struct{
+
+	Id 					int64 `json:"id"`
+	Title 			string `json:"title"`
+	Description string `json: "decription"`
+	Done 				bool `	json:"done`
+
+
+}
