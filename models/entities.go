1fe72fcd4b5e8a08aad6a2909d3186c24692bf41
jose-cleiton <cleitons835@gmail.com>
2023-01-01 21:10:10 -0300

    feat: :sparkles: Criando a função Get
    Este código é uma função em Go que obtém um item de "todo" a partir de um banco de dados. A função recebe um ID inteiro como entrada e retorna uma struct Todo e um valor de erro.  A função começa abrindo uma conexão com o banco de dados usando a função OpenConnection do pacote db. Se houver um erro ao abrir a conexão, ela retorna imediatamente com o valor de erro. Caso contrário, ela continua usando uma declaração defer para fechar a conexão assim que a função for completada.  Em seguida, a função usa o método QueryRow da conexão para executar uma instrução SELECT que recupera uma única linha da tabela todos onde a coluna id é igual ao ID fornecido como entrada. A linha resultante é armazenada na variável row.  Por fim, a função usa o método Scan da variável row para ler os valores da linha para os campos da struct Todo. Se houver um erro ao ler os valores, ele é retornado como o segundo valor de retorno da função.

--- models/entities.go
+++ models/entities.go
@@ -5,7 +5,7 @@ package models
 
 type Todo struct{
 
-	Id 					int64 	`json:"id"`
+	ID				int64 	`json:"id"`
 	Title 			string 	`json:"title"`
 	Description string 	`json:"description"`
 	Done 				bool 		`json:"Done"`
---
+++ models/get.go
@@ -0,0 +1,21 @@
+package models
+
+import "Api/db"
+
+
+
+func Get(id int64)(todo Todo, err error){
+
+	conn, err := db.OpenConnection()
+	if err!= nil{
+		return
+	}
+	defer conn.Close()
+
+	row := conn.QueryRow(`SELECT * FROM todos WHERE id = $1`, id)
+
+	err = row.Scan(&todo.ID, &todo.Title, &todo.Description, &todo.Done)
+
+	return
+
+}
